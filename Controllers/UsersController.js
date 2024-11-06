6088b04275a31d32bc2712ae33abe9d6971b2dc4
Amresh Kumar <akumarcpr021@gmail.com>
2024-11-06 21:44:55 +0530

    Your commit message

--- Controllers/UsersController.js
+++ Controllers/UsersController.js
@@ -1,5 +1,5 @@
 import { Users } from "../Models/UsersModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
